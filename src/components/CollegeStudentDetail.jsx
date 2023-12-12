ccd85a56d1318dc29cbdd4974ebe14cfde6cea82
C0SMlC <pendurkarpratik04@gmail.com>
2023-12-12 10:31:52 +0530

    Refactor CollegeStudentDetail component

--- src/components/CollegeStudentDetail.jsx
+++ src/components/CollegeStudentDetail.jsx
@@ -8,8 +8,9 @@ import {
   Tbody,
   Tr,
   Th,
-  VStack,
   Td,
+  VStack,
+  Heading,
   Box,
   Text,
   Link,
@@ -18,7 +19,6 @@ import { Link as RouterLink } from "react-router-dom";
 
 import PageNav from "./PageNav";
 import useAuth from "../hooks/useAuth";
-import CollegeStudentItem from "./CollegeStudentItem";
 
 const StudentsTable = ({ selectedCollege }) => {
   const authUser = useAuth();
@@ -93,9 +93,9 @@ const StudentsTable = ({ selectedCollege }) => {
           justify="center"
         >
           <VStack spacing={6} align="center" mt={8}>
-            <Text fontSize="5xl" mb={10}>
-              {students.length || ""} users from {selectedCollege}
-            </Text>
+            <Heading size="xl" mb={10}>
+              Users From {selectedCollege}
+            </Heading>
             <Box overflowX="auto" w="100%">
               {isLoading ? (
                 // Display a loading spinner while data is being fetched
@@ -116,19 +116,47 @@ const StudentsTable = ({ selectedCollege }) => {
                     </Tr>
                   </Thead>
                   <Tbody>
-                    {students.length > 0 ? (
-                      students.map((student, index) => (
-                        <CollegeStudentItem
-                          student={student}
-                          index={index}
-                          key={index}
-                        />
-                      ))
-                    ) : (
-                      <Tr>
-                        <Td textAlign="center">No Students Found</Td>
+                    {students.map((student, index) => (
+                      <Tr key={index}>
+                        <Td textAlign="center">{index + 1}</Td>
+                        <Td textAlign="center">
+                          {student.first_name + " " + student.last_name}
+                        </Td>
+                        <Td textAlign="center">{student.role}</Td>
+                        <Td textAlign="center">{student.email_id}</Td>
+                        <Td textAlign="center">
+                          <a
+                            href={student.github_profile || "#"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {student.github_profile || "Not Updated"}
+                          </a>
+                        </Td>
+                        <Td textAlign="center">
+                          <a
+                            href={student.website || "#"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {student.website || "Not Updated"}
+                          </a>
+                        </Td>
+                        <Td textAlign="center">
+                          {student.user_interests
+                            .map(
+                              (interest) => interest.area_of_interest.interest
+                            )
+                            .join(", ")}
+                        </Td>
+                        <Td textAlign="center">
+                          {student.user_skills
+                            .map((skillObject) => skillObject.skill)
+                            .join(", ") || "Not Updated"}
+                        </Td>
+                        <Td textAlign="center">{student.numProjects}</Td>
                       </Tr>
-                    )}
+                    ))}
                   </Tbody>
                 </Table>
               )}
@@ -160,10 +188,6 @@ const StudentsTable = ({ selectedCollege }) => {
 
 StudentsTable.propTypes = {
   selectedCollege: PropTypes.string,
-};
-
-CollegeStudentItem.defaultProps = {
-  student: null,
 };
 
 export default StudentsTable;
